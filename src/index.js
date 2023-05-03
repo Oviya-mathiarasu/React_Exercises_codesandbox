49cd67e68287018b8b42343a39a7a48e98617a33
Oviya M <87748843+Oviya-mathiarasu@users.noreply.github.com>
2023-05-03 16:26:44 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const fn = 7;
+ReactDOM.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>your favorite number is {fn}</p>
+  </div>,
+  document.getElementById("root")
+);
